317142665d17bd7f163a975a3db8bcea67e4a31a
sapphi-red <green@sapphi.red>
2021-12-27 16:18:28 +0900

    webm(vp9) => webm(av1)

--- server/convert_gif2webm.js
+++ server/convert_gif2webm.js
@@ -1,8 +1,9 @@
 import fs from 'fs/promises'
 import path from 'path'
-import { fetchFile } from '@ffmpeg/ffmpeg'
+// import { fetchFile } from '@ffmpeg/ffmpeg'
 import { PUBLIC_PATH } from './src/paths.js'
 import { ffmpeg } from './src/ffmpeg.js'
+import { execSync } from 'child_process'
 
 const doneFiles = []
 
@@ -22,11 +23,14 @@ const doneFiles = []
 
     const newFile = `${name}.webm`
 
-    const buf = await fetchFile(path.resolve(dir, file))
-    ffmpeg.FS('writeFile', 'file', buf)
-    await ffmpeg.run('-i', 'file', '-movflags', 'faststart', '-vf', 'scale=640:-1', '-pix_fmt', 'yuv420p', '-row-mt', '1', newFile)
-    const exported = ffmpeg.FS('readFile', newFile)
-    await fs.writeFile(path.resolve(dir, `./${newFile}`), exported, 'binary')
+    // const buf = await fetchFile(path.resolve(dir, file))
+    // ffmpeg.FS('writeFile', 'file', buf)
+    // await ffmpeg.run('-i', 'file', '-movflags', 'faststart', '-vf', 'scale=640:-1', '-pix_fmt', 'yuv420p', '-row-mt', '1', newFile)
+    // const exported = ffmpeg.FS('readFile', newFile)
+    // await fs.writeFile(path.resolve(dir, `./${newFile}`), exported, 'binary')
+
+    execSync(`"C:\\Users\\green\\Downloads\\ffmpeg-4.4.1-full_build\\bin\\ffmpeg.exe" -i ${path.resolve(dir, file)} -movflags faststart -vf "scale=640:-1" -vcodec libaom-av1 -pix_fmt yuv420p ${path.resolve(dir, `./${newFile}`)}`)
+
     console.log(`outputed: ${newFile}`)
   }
 })()

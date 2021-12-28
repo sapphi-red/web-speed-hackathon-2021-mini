e3de2fdd1583126f5c00e9ff7a1e2a4e8bc9e4aa
sapphi-red <green@sapphi.red>
2021-12-28 21:09:13 +0900

    push fetch result and preload navigation fonts

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -16,4 +16,15 @@ requestIdleCallback(() => {
   $font.rel = 'stylesheet'
   $font.href = '/fonts/webfont.css'
   document.head.append($font)
+
+  // ナビゲーションバーで使われるフォント
+  const criticalFonts = ['bold/000', 'bold/003', 'bold/010', 'bold/011', 'bold/013']
+  for (const cf of criticalFonts) {
+    const $preload = document.createElement('link')
+    $preload.rel = 'preload'
+    $preload.as = 'font'
+    $preload.crossOrigin = ''
+    $preload.href = `/fonts/genei-m-gothic/${cf}.woff2`
+    document.head.append($preload)
+  }
 })

6befb271100c1658bcd9beeab38bd0e773e022b7
sapphi-red <green@sapphi.red>
2021-12-28 22:27:31 +0900

    use preact

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -86,6 +86,12 @@ const config = {
     }),
   ],
   resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',     // Must be below test-utils
+      'react/jsx-runtime': 'preact/jsx-runtime'
+    },
     extensions: ['.js', '.jsx'],
     fallback: {
       fs: false,
